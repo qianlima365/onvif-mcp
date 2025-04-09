b984c4087688819441c6399f54dc7cfce7e7cd1d
mywangxiaojie <1873297122@qq.com>
2025-04-09 16:20:44 +0800

    新增了Dockerfile文件，调整了main.py

--- main.py
+++ main.py
@@ -121,40 +121,5 @@ async def focus_move(host: str, port: int = 80, usr: str = 'admin', pwd: str = '
     client = OnvifClient(host, port, usr, pwd, needSnapImg=False)
     return client.focus_move()
 
-
-
-
-def main():
-    print("Hello from onvif-mcp!")
-
-    # 设备发现
-    print(ws_discovery())
-
-    # client = OnvifClient('10.17.20.88', 80, 'admin', 'admin123', needSnapImg=False)
-    # client = OnvifClient('10.17.20.77', 80, 'admin', 'admin123', needSnapImg=False)
-    # client = OnvifClient('10.17.20.66', 80, 'admin', 'admin123', needSnapImg=False)
-    client = OnvifClient('10.17.20.110', 80, 'admin', 'qwer1234', needSnapImg=False)
-    # 如果要控制特定摄像头，可以下边这样写
-    # client = OnvifClient('192.168.1.10', 80, 'admin', '123456', token="ProfileToken002", sourceToken= "VideoSourceToken002", nodeToken="NodeToken002", needSnapImg=False)
-
-    # 获取所有画面所有码流的RTSP地址、token(即ProfileToken)、sourceToken、nodeToken等信息
-
-    # 获取设备信息
-    print(json.dumps(client.get_deviceInfo()))
-
-    # # 设置时间
-    # client.set_cam_time()
-
-    # 云台与聚焦控制
-    # 云台上移
-    ptzChangeByClient(client, 'Down', 1)
-    # 移动一秒
-    time.sleep(10)
-    # 然后停止
-    ptzChangeByClient(client, 'Up', 0)
-    time.sleep(10)
-
-
 if __name__ == "__main__":
-    # main()
     app.run(transport='stdio')
